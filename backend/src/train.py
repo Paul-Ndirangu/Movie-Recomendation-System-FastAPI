6c97cf2910cd1fd2a3cc9dd5e6bdd172ec59651f
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-09 20:01:09 +0300

    train.py commit

--- backend/src/train.py
+++ backend/src/train.py
@@ -1,8 +1,9 @@
 import tez
 import pandas as pd
-from sklearn import model_selection
-
+from sklearn import model_selection, metrics, preprocessing
 import torch
+import torch.nn as nn
+import numpy as np
 
 
 class MovieDataset:
@@ -21,16 +22,58 @@ class MovieDataset:
         rating = self.ratings[item]
         
         return {
-            "user": torch.tensor(user, dtype=torch.long),
-            "movie": torch.tensor(movie, dtype=torch.long),
-            "rating": torch.tensor(rating, dtype=torch.float),
+            "users": torch.tensor(user, dtype=torch.long),
+            "movies": torch.tensor(movie, dtype=torch.long),
+            "ratings": torch.tensor(rating, dtype=torch.float),
             }
+
+
+class RecSysModel(tez.Model):
+    def __init__(self, num_users, num_movies):
+        super().__init__()
+        self.user_embed = nn.Embedding(num_users, 32)
+        self.movie_embed = nn.Embedding(num_movies, 32)
+        self.out = nn.Linear(64, 1)
+        self.step_scheduler_after = "epoch"
+        
+    def fetch_optimizer(self):
+        opt = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return opt
+    
+    def fetch_scheduler(self):
+        sch = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=0.7)
+        return sch
+    
+    def monitor_metrics(self, output, rating):
+        output = output.detach().cpu().numpy()
+        rating = rating.detach().cpu().numpy()
+        return {
+            "rmse":np.sqrt(metrics.mean_squared_error(rating, output))
+        }
+       
+    def forward(self, users, movies, ratings=None):
+        user_embeds = self.user_embed(users)
+        movie_embeds = self.movie_embed(movies)
+        output = torch.cat([user_embeds, movie_embeds])
+        output = self.out(output)
+
+        loss = nn.MSELoss()(output, ratings.view(-1,1))
+        calc_metrics = self.monitor_metrics(output, ratings.view(-1,1))
+        return output, loss, calc_metrics
 
 
 
 def train():
     df = pd.read_csv('/home/paul/Mindscope/Mindscope/Movie-Recomendation-System-FastAPI-React-/backend/ml-latest-small/ratings.csv')
     # ID, user, movie, rating
+    
+    lbl_user = preprocessing.LabelEncoder()
+    lbl_movie = preprocessing.LabelEncoder()
+    
+    df_user = lbl_user.fit_transform(df.userId.values)
+    df_movie = lbl_movie.fit_transform(df.movieId.values)
+    
+    
     df_train, df_valid = model_selection.train_test_split(
         df, test_size=0.1, random_state=42, stratify=df.rating.values
         )
@@ -47,6 +90,19 @@ def train():
         ratings= df_valid.rating.values 
         )
 
+    model = RecSysModel(
+        num_users= len(lbl_user.classes_), 
+        num_movies= len(lbl_movie.classes_)
+        )
+    
+    device = "cpu"
+    model.to(device)
+
+    model.fit(
+        train_dataset, valid_dataset,
+        train_bs = 1024, valid_bs = 1024,
+        fp16 = True
+    )
 
 if __name__ == '__main__':
     train()
